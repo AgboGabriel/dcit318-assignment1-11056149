cdd02ef0a4306d279ac55bcfdb7d677bb97c04a5
AgboGabriel <150946746+AgboGabriel@users.noreply.github.com>
2025-08-04 16:21:08 +0000

    Ticket Price Calculator
    this is the ticket price calculator program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class program
+{
+    static int TicketPriceCalculator(int price, int age)
+    {
+
+        if (age <= 12)
+        {
+            price = price - 3;
+
+        }
+        else if (age >= 60)
+        {
+            price = price - 3;
+        }
+        return price;
+
+        }
+    static void Main(String[] args)
+    {
+        int price = 10;
+        Console.WriteLine("Enter your age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        int finalPrice = TicketPriceCalculator(price, age);
+        Console.WriteLine("The final ticket price is: " + finalPrice);
+    }
+}
